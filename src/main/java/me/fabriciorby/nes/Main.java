c5384c8a9691f6b27d58b95d9af2cbe5d938257f
fabriciorby <fabriciorby@hotmail.com>
2024-06-17 19:39:57 +0200

    change to JavaFX raiz

--- src/main/java/me/fabriciorby/nes/Main.java
+++ src/main/java/me/fabriciorby/nes/Main.java
@@ -1,148 +1,27 @@
 package me.fabriciorby.nes;
 
-import org.lwjgl.Version;
-import org.lwjgl.glfw.GLFWErrorCallback;
-import org.lwjgl.glfw.GLFWVidMode;
-import org.lwjgl.opengl.GL;
-import org.lwjgl.system.MemoryStack;
-import org.lwjgl.system.MemoryUtil;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-import java.awt.*;
-import java.nio.IntBuffer;
+public class Main extends Application {
 
-import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.stackPush;
-import static org.lwjgl.system.MemoryUtil.NULL;
-
-public class Main {
-    // The window handle
-    private long window;
-
-    public void run() {
-        System.out.println("Hello LWJGL " + Version.getVersion() + "!");
-
-        init();
-        loop();
-
-        // Free the window callbacks and destroy the window
-        glfwFreeCallbacks(window);
-        glfwDestroyWindow(window);
-
-        // Terminate GLFW and free the error callback
-        glfwTerminate();
-        glfwSetErrorCallback(null).free();
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Hello World");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        StackPane pane = new StackPane();
+        pane.getChildren().add(btn);
+        Scene scene = new Scene(pane, 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
 
-    private void init() {
-        GLFWErrorCallback.createPrint(System.err).set();
-
-        if ( !glfwInit() )
-            throw new IllegalStateException("Unable to initialize GLFW");
-
-        glfwDefaultWindowHints(); // optional, the current window hints are already the default
-        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE); // the window will stay hidden after creation
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE); // the window will be resizable
-
-        window = glfwCreateWindow(256*4, 240*4, "Hello World!", NULL, NULL);
-        if ( window == NULL )
-            throw new RuntimeException("Failed to create the GLFW window");
-
-        // Setup a key callback. It will be called every time a key is pressed, repeated or released.
-        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-                glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-        });
-
-        // Get the thread stack and push a new frame
-        try ( MemoryStack stack = stackPush() ) {
-            IntBuffer pWidth = stack.mallocInt(1); // int*
-            IntBuffer pHeight = stack.mallocInt(1); // int*
-
-            // Get the window size passed to glfwCreateWindow
-            glfwGetWindowSize(window, pWidth, pHeight);
-
-            // Get the resolution of the primary monitor
-            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
-
-            // Center the window
-            glfwSetWindowPos(
-                    window,
-                    (vidmode.width() - pWidth.get(0)) / 2,
-                    (vidmode.height() - pHeight.get(0)) / 2
-            );
-        } // the stack frame is popped automatically
-
-        glfwMakeContextCurrent(window);
-        glfwSwapInterval(1);
-        glfwShowWindow(window);
-
-    }
-
-    private void loop() {
-        GL.createCapabilities();
-//        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
-
-        /* Declare buffers for using inside the loop */
-        IntBuffer width = MemoryUtil.memAllocInt(1);
-        IntBuffer height = MemoryUtil.memAllocInt(1);
-
-//        int vao = glGenVertexArrays();
-//        glBindVertexArray(vao);
-
-        while ( !glfwWindowShouldClose(window) ) {
-            float ratio;
-
-            /* Get width and height to calcualte the ratio */
-            glfwGetFramebufferSize(window, width, height);
-            ratio = width.get() / (float) height.get();
-
-            /* Rewind buffers for next get */
-            width.rewind();
-            height.rewind();
-
-            /* Set viewport and clear screen */
-            glViewport(0, 0, width.get(), height.get());
-            glClear(GL_COLOR_BUFFER_BIT);
-
-            /* Set ortographic projection */
-            glMatrixMode(GL_PROJECTION);
-            glLoadIdentity();
-            glOrtho(-ratio, ratio, -1f, 1f, 1f, -1f);
-            glMatrixMode(GL_MODELVIEW);
-
-            /* Rotate matrix */
-            glLoadIdentity();
-            glRotatef((float) glfwGetTime() * 50f, 0f, 0f, 1f);
-
-            /* Render triangle */
-            glBegin(GL_TRIANGLES);
-            glColor3f(1f, 0f, 0f);
-            glVertex3f(-0.6f, -0.4f, 0f);
-            glColor3f(0f, 1f, 0f);
-            glVertex3f(0.6f, -0.4f, 0f);
-            glColor3f(0f, 0f, 1f);
-            glVertex3f(0f, 0.6f, 0f);
-            glEnd();
-
-            /* Swap buffers and poll Events */
-            glfwSwapBuffers(window);
-            glfwPollEvents();
-
-            /* Flip buffers for next loop */
-            width.flip();
-            height.flip();
-
-        }
-
-        /* Free buffers */
-        MemoryUtil.memFree(width);
-        MemoryUtil.memFree(height);
-
-    }
 
     public static void main(String[] args) {
-        new Main().run();
+        launch(args);
     }
 }
