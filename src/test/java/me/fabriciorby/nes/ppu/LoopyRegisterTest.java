c05e5f7c19fadc724010d4946ab906373e156c46
fabriciorby <fabriciorby@hotmail.com>
2024-08-22 22:42:18 +0200

    add some more assertions for loopy register

--- src/test/java/me/fabriciorby/nes/ppu/LoopyRegisterTest.java
+++ src/test/java/me/fabriciorby/nes/ppu/LoopyRegisterTest.java
@@ -18,7 +18,7 @@ class LoopyRegisterTest {
         loopyRegister.set(LoopyRegister.Loopy.COARSE_X, 0b01100);
 
         assertEquals(0b0000111000101100, loopyRegister.loopyRegister);
-        assertEquals(loopyRegister.get(LoopyRegister.Loopy.COARSE_Y), 0b10001);
+        assertEquals(loopyRegister.get(LoopyRegister.Loopy.COARSE_X), 0b01100);
         assertEquals(loopyRegister.get(LoopyRegister.Loopy.COARSE_Y), 0b10001);
         assertEquals(loopyRegister.get(LoopyRegister.Loopy.NAMETABLE_Y), 0b1);
         assertEquals(loopyRegister.get(LoopyRegister.Loopy.NAMETABLE_X), 0b1);
@@ -36,12 +36,22 @@ class LoopyRegisterTest {
         loopyRegister.set(LoopyRegister.Loopy.NAMETABLE_Y, 0b0);
 
         assertEquals(0b1111001111111111, loopyRegister.loopyRegister);
-        assertEquals(loopyRegister.get(LoopyRegister.Loopy.COARSE_Y), 0b11111);
+        assertEquals(loopyRegister.get(LoopyRegister.Loopy.COARSE_X), 0b11111);
         assertEquals(loopyRegister.get(LoopyRegister.Loopy.COARSE_Y), 0b11111);
         assertEquals(loopyRegister.get(LoopyRegister.Loopy.NAMETABLE_Y), 0b0);
         assertEquals(loopyRegister.get(LoopyRegister.Loopy.NAMETABLE_X), 0b0);
         assertEquals(loopyRegister.get(LoopyRegister.Loopy.FINE_Y), 0b111);
         assertEquals(loopyRegister.get(LoopyRegister.Loopy.UNUSED), 0b1);
+        printLoopy();
+
+        loopyRegister.set(LoopyRegister.Loopy.COARSE_Y, loopyRegister.get(LoopyRegister.Loopy.COARSE_Y) + 1);
+        assertEquals(loopyRegister.get(LoopyRegister.Loopy.COARSE_X), 0b11111);
+        assertEquals(loopyRegister.get(LoopyRegister.Loopy.COARSE_Y), 0b00000);
+        printLoopy();
+
+        loopyRegister.set(LoopyRegister.Loopy.COARSE_X, loopyRegister.get(LoopyRegister.Loopy.COARSE_X) + 1);
+        assertEquals(loopyRegister.get(LoopyRegister.Loopy.COARSE_X), 0b00000);
+        assertEquals(loopyRegister.get(LoopyRegister.Loopy.COARSE_Y), 0b00000);
         printLoopy();
     }
 
